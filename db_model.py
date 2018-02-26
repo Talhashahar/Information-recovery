e1b2e4e4db06163a00107c0459a2cab556753e5e
Talhashahar <talhashahar@gmail.com>
2018-02-26 21:04:58 +0200

    Add files via upload

--- db_model.py
+++ db_model.py
@@ -10,7 +10,7 @@ def get_connection():
     if cnx is None:
         cnx = mysql.connector.connect(user='root', password='Password1',
                                       host='127.0.0.1',
-                                      database='db_final_project', port=3306)
+                                      database='googlev2', port=3306)
     return cnx
 
 
@@ -18,3 +18,11 @@ def disconnectDB():
     db = get_connection()
     db.close()
 
+
+def get_unuse_word_from_db():
+    query = "SELECT * FROM googlev2.unuse_words;"
+    cursor = get_connection().cursor()
+    cursor.execute(query)
+    return_values = []
+    return_values = cursor.fetchall()
+    return return_values
--- index_model.py
+++ index_model.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import docx2txt
 
 ''' configutions '''
 folder_before_index = 'C:\\Users\\talha_000\\PycharmProjects\\GoogleV2\\data\\Original\\'
@@ -24,3 +25,27 @@ def index_files_from_temp_folder():
     global folder_temp_index
     global folder_after_index
     file_list = os.listdir(folder_temp_index)
+    for file_name in file_list:
+        text = file_to_text(file_name)
+        if text is None:
+            continue
+
+
+
+def file_to_text(file_name):
+    global folder_temp_index
+    global folder_after_index
+    file_type = file_name.split(".")
+    if file_type == "txt":
+        file = open(folder_temp_index+file_name, "r")
+        text_from_file = file.read()
+        return text_from_file
+    elif file_type == "doc":
+        text_from_file = docx2txt.process(folder_temp_index + file_name)
+        return text_from_file
+    elif file_type == "docx":
+        text_from_file = docx2txt.process(folder_temp_index+file_name)
+        return text_from_file
+    else:
+        print 'write to log the file cant be read'
+        return None
